ccab49b3c1c29fda12b5e01c57d096589b6eead8
Nikita <kucenko.nikita@gmail.com>
2023-03-11 02:33:51 +0700

    Сделал стили для кнопки

--- src/components/UI/CustomButton.vue
+++ src/components/UI/CustomButton.vue
@@ -17,6 +17,12 @@ const props = defineProps({
     required: true,
     default: false,
   },
+  buttonType: {
+    type: String,
+    required: false,
+    default: 'primary',
+    validator: (value: string) => ['primary', 'secondary', 'tertiary'].includes(value),
+  },
 });
 
 const onClickHandler = () => {
@@ -27,7 +33,36 @@ const onClickHandler = () => {
 <style scoped lang="scss">
 @import "@/styles/_base.scss";
 
+
 .custom-button {
   font-family: 'Roboto Mono', monospace;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  padding: 8px 12px;
+  cursor: pointer;
+  transition: all 0.2s ease;
+
+  &:hover:not(:disabled) {
+    background-color: darken($primary-color, 5%);
+    color: $white;
+  }
+
+  &.secondary {
+    color: $primary-color;
+    background-color: transparent;
+    border: 1px solid $primary-color;
+  }
+
+  &.tertiary {
+    color: $primary-color;
+    background-color: transparent;
+    border: none;
+  }
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
 }
 </style>
