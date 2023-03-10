1f011b7d66ae3abad130b601243bb8dbf1f62e95
Nikita <kucenko.nikita@gmail.com>
2023-03-11 02:28:23 +0700

    Сделал форму регистрации

---
+++ src/components/UI/CustomButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    class="custom-button"
+    @click="onClickHandler"
+    :disabled="props.disabled"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["click"]);
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+const onClickHandler = () => {
+  emit("click");
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/_base.scss";
+
+.custom-button {
+  font-family: 'Roboto Mono', monospace;
+}
+</style>
--- src/views/PublicPages/RegistrationPage.vue
+++ src/views/PublicPages/RegistrationPage.vue
@@ -1,32 +1,42 @@
 <template>
   <div class="registration-page">
-    Registration
-    <button @click="registrationRequest">Registration</button>
+    <RegistrationForm />
+    <!--    Registration-->
+    <!--    <button @click="registrationRequest">Registration</button>-->
   </div>
 </template>
 
 <script setup lang="ts">
-import {registration} from "@/api/userServices/userService";
-import {RegistrationData} from "@/api/userServices/types";
-import {AxiosError} from "axios";
-import {reactive} from "vue";
+import { registration } from "@/api/userServices/userService";
+import { RegistrationData } from "@/api/userServices/types";
+import { AxiosError } from "axios";
+import { reactive } from "vue";
+import RegistrationForm from "@/components/registration/RegistrationForm.vue";
 
-const errors: Array<AxiosError> = reactive([])
+const errors: Array<AxiosError> = reactive([]);
 
 const registrationRequest = async () => {
   try {
     const registrationData: RegistrationData = {
       email: "nikita@gmail.com",
       password: "password",
-      username: "Nikita"
-    }
+      username: "Nikita",
+    };
     const response = await registration(registrationData);
   } catch (e: any) {
     errors.push(e);
   }
-}
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "@/styles/_base.scss";
 
+.registration-page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
